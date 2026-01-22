9abc62850e4796ee252e512f47a4bb64a7f49237
GianluigiCandido <GianluigiCandido@PC-CANDIDO>
2026-01-22 12:20:52 +0100

    Add Project

---
+++ Test Suites/TS_001_prova.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_001_prova</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d59c3757-9a40-430a-810a-a5ed8d154bdc</testSuiteGuid>
+   <testCaseLink>
+      <guid>bdb91d91-dc55-42bb-b177-dee6829f63ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001_prova</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
